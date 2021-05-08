b3d97f934277f87f454c33228d4a123bc0f9aebf
leo <leo.ertuna@gmail.com>
2021-05-09 01:18:12 +0300

    Added body to proxy request

--- proxy-shared-message-protocol/worker_proxy_message_protocol/proxy_request.py
+++ proxy-shared-message-protocol/worker_proxy_message_protocol/proxy_request.py
@@ -12,7 +12,8 @@ class ProxyRequest(BaseModel):
     query_params: Dict[str, Any]
     path_params: Dict[str, Any]
     cookies: Dict[str, Any]
+    body: Any
     client: Tuple[str, str]
 
-    def __init__(self, method: str, url: str, base_url: str, headers: Dict[str, Any], query_params: Dict[str, Any], path_params: Dict[str, Any], cookies: Dict[str, Any], client: Tuple[str, str]) -> None:
-        super().__init__(method=method, url=url, base_url=base_url, headers=headers, query_params=query_params, path_params=path_params, cookies=cookies, client=client)
+    def __init__(self, method: str, url: str, base_url: str, headers: Dict[str, Any], query_params: Dict[str, Any], path_params: Dict[str, Any], cookies: Dict[str, Any], body: Any, client: Tuple[str, str]) -> None:
+        super().__init__(method=method, url=url, base_url=base_url, headers=headers, query_params=query_params, path_params=path_params, cookies=cookies, body=body, client=client)
