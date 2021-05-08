1e6349857286c265b7c7dfa011b328565e94bad2
leo <leo.ertuna@gmail.com>
2021-05-09 00:46:03 +0300

    Changed to request/response id

--- proxy-shared-message-protocol/worker_proxy_message_protocol/proxy_channel.py
+++ proxy-shared-message-protocol/worker_proxy_message_protocol/proxy_channel.py
@@ -5,8 +5,8 @@ from simplestr import gen_str_repr
 @gen_str_repr
 class ProxyChannel(BaseModel):
     id: str
-    request_channel: str
-    response_channel: str
+    request_id: str
+    response_id: str
 
-    def __init__(self, id: str, request_channel: str, response_channel: str) -> None:
-        super().__init__(id=id, request_channel=request_channel, response_channel=response_channel)
+    def __init__(self, id: str, request_id: str, response_id: str) -> None:
+        super().__init__(id=id, request_id=request_id, response_id=response_id)
