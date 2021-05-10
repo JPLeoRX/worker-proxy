bdff4d7cc4855e1ecd0a0ce3407551c5252f6577
leo <leo.ertuna@gmail.com>
2021-05-10 03:50:55 +0300

    Include proxied url in env utils

--- proxy-shared-utils/worker_proxy_utils/utils_env.py
+++ proxy-shared-utils/worker_proxy_utils/utils_env.py
@@ -10,6 +10,7 @@ class UtilsEnv:
         self.worker_proxy_rabbitmq_port = -1
         self.worker_proxy_rabbitmq_username = ''
         self.worker_proxy_rabbitmq_password = ''
+        self.worker_proxy_client_proxied_base_url = ''
         self.__reload()
 
     def __reload(self):
@@ -19,7 +20,7 @@ class UtilsEnv:
             print('UtilsEnv.__reload(): Warning! WORKER_PROXY_RABBITMQ_HOST not found')
 
         if 'WORKER_PROXY_RABBITMQ_PORT' in os.environ:
-            self.worker_proxy_rabbitmq_port = os.environ['WORKER_PROXY_RABBITMQ_PORT']
+            self.worker_proxy_rabbitmq_port = int(os.environ['WORKER_PROXY_RABBITMQ_PORT'])
         else:
             print('UtilsEnv.__reload(): Warning! WORKER_PROXY_RABBITMQ_PORT not found')
 
@@ -32,6 +33,11 @@ class UtilsEnv:
             self.worker_proxy_rabbitmq_password = os.environ['WORKER_PROXY_RABBITMQ_PASSWORD']
         else:
             print('UtilsEnv.__reload(): Warning! WORKER_PROXY_RABBITMQ_PASSWORD not found')
+
+        if 'WORKER_PROXY_CLIENT_PROXIED_BASE_URL' in os.environ:
+            self.worker_proxy_client_proxied_base_url = os.environ['WORKER_PROXY_CLIENT_PROXIED_BASE_URL']
+        else:
+            print('UtilsEnv.__reload(): Warning! WORKER_PROXY_CLIENT_PROXIED_BASE_URL not found')
 
     def get_rabbitmq_config(self) -> RabbitmqConfig:
         return RabbitmqConfig(self.worker_proxy_rabbitmq_host, self.worker_proxy_rabbitmq_port, self.worker_proxy_rabbitmq_username, self.worker_proxy_rabbitmq_password)
